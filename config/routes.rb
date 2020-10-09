43dbe532d018ab90ec89862c26fd0ecbf4978364
Vmcg22 <54409821+Vmcg22@users.noreply.github.com>
2020-10-09 17:38:50 -0500

    Se creó controlador (Patients con los siguientes métodos: index y show)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'patients/index'
+  get 'patients/show'
   #get 'users/new', to: 'users#new'
   #get 'users/create'
   
   get 'welcome/index'
   root 'welcome#index'
+  #resource :patients, only: [:index, :show]
 
   resource :users, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
