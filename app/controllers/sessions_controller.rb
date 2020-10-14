c4c5ece9ee47f10b35f2adca8857e431fb654715
Vmcg22 <54409821+Vmcg22@users.noreply.github.com>
2020-10-13 21:19:01 -0500

    Se hizó la lógica para que funcione la validación de cuentas al momento de iniciar sesión

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to welcome_main_path, notice: 'Sesión Iniciada'
+    else
+      flash.now[:alert] = 'El Correo o Contraseña es Incorrecto'
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to welcome_main_path, notice: 'Sesión Cerrada'
   end
 end
