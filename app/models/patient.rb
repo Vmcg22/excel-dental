01e4bd2df4084bdb31253cb5011e15512822ef92
Vmcg22 <54409821+Vmcg22@users.noreply.github.com>
2020-10-19 15:58:48 -0500

    Se integraron validaciones en todos los campos para (views/patients/new.html.erb)

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,4 @@
 class Patient < ApplicationRecord
     #Validaciones del modelo:
-    validates :nombres, :apellidos, :edad, :fecha_nacimiento, :colonia, :direccion, :telefono, presence: true
+    validates :nombres, :apellidos, :edad, :fecha_nacimiento, :colonia, :direccion, :celular, presence: true
 end
